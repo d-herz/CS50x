9552a2627a51a9d50a72b29085b2b414ca10b1a0
Daniel Hyres <danhyres@gmail.com>
2023-04-20 15:58:30 -0400

    addresses

--- week4/addresses.c
+++ week4/addresses.c
@@ -5,3 +5,8 @@ int main(void)
   int n = 50;
   printf("%i\n", n);
 }
+
+int x = 42; // declare an integer variable and initialize it with the value 42
+int *ptr;   // declare a pointer variable that can store the memory address of an integer
+
+ptr = &x; // assign the memory address of x to the pointer variable ptr
