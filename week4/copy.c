7a7e68a748db14af5eda8ce928956c65a62e3ef4
Daniel Hyres <danhyres@gmail.com>
2023-04-26 16:05:03 -0400

    Copy using malloc

---
+++ week4/copy.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+  char *s = get_string("s: ");
+
+  // this is like copying the same ADDRESS
+  char *t = s;
+
+  if (strlen(t) > 0)
+  {
+    t[0] = toupper(t[0]);
+  }
+
+  // both will be uppercase
+  printf("s: %s\n", s);
+  printf("t: %s\n", t);
+}
