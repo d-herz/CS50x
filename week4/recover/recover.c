3dec6d5c14a4b8633e6e648b10253b7853979f79
Daniel Hyres <danhyres@gmail.com>
2023-05-13 11:36:27 -0400

    File recovery program

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
+// Accept 1 command line arg
 int main(int argc, char *argv[])
 {
- 
+  if (argc <= 1)
+  {
+    printf("Usage: File name\n");
+    return 1;
+  }
+
+  FILE *file = fopen(argv[1], "r");
+
+  if (file == NULL)
+  {
+    printf("Filename: '%s' unopenable\n", argv[1]);
+    return 1;
+  }
+  else
+  {
+    printf("Filename: '%s' opened\n", argv[1]);
+  }
 }
