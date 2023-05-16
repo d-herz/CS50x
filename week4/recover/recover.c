89ba9ea2c64f845023bca983b79d3604db47f872
Daniel Hyres <danhyres@gmail.com>
2023-05-16 11:45:10 -0400

    Recover submission

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Accept 1 command line arg
 int main(int argc, char *argv[])
 {
-  // Ensure command line args exist
   if (argc <= 1)
   {
-    printf("Usage: File name\n");
+    printf("Usage: ./recover IMAGE\n");
     return 1;
   }
 
-  // Open input file
+  // Open memory card
   FILE *input_file = fopen(argv[1], "r");
-  // Check for nullness
   if (input_file == NULL)
   {
     printf("Filename: '%s' unopenable\n", argv[1]);
@@ -24,39 +21,54 @@ int main(int argc, char *argv[])
     printf("Filename: '%s' opened\n", argv[1]);
   }
 
-  // TODO's below
-  // Each jpg starts with header: 0xff, 0xd8, 0xff, and ends with 0xe(0-f)
-  // Look through file/blocks for begining headers
-  // Once found:
-  // Open new file to start writing to, block x block until you find another beginning header
-
-  // Need to name new output file and increment it each time
   int image_num = 0;
   char *img_ext = ".jpg";
-  char file_name[10];
+  char file_name[15];
+  unsigned char buffer[512];
 
-  sprintf(file_name, "%03d%s", image_num, img_ext);
-  printf("Filename: %s\n", file_name);
+  FILE *output = NULL;
 
-  FILE *output = fopen("000.jpg", "w");
-  if (output == NULL)
+  while (fread(buffer, 1, 512, input_file))
   {
-    printf("Failed to open the file.\n");
-    fclose(input_file);
-    return 1;
-  }
-  else
-  {
-    printf("Filename: %s opened.\n", file_name);
-    while (fread(output, 1, 512, input_file))
+    // If start of new JPEG, check if first jpeg or not, and start writing
+    if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
     {
-      printf("%p\n", output);
+      if (output != NULL)
+      {
+        fclose(output);
+        output = NULL;
+        image_num++;
+      }
+      // Print formatted string to memory (and terminal, to check)
+      sprintf(file_name, "%03d%s", image_num, img_ext);
+      printf("Filename: %s created\n", file_name);
+
+      // Open file with new filename (and print to terminal)
+      output = fopen(file_name, "w");
+      printf("Filename: %s opened\n", file_name);
+
+      // Write to new file, and print to terminal
+      fwrite(buffer, 1, 512, output);
+      printf("Filename: %s written too\n", file_name);
+
+      // Check if new file is empty, if so then an error has occured
+      if (output == NULL)
+      {
+        printf("Failed to open the file.\n");
+        fclose(input_file);
+        return 1;
+      }
+
+      // Continue reading and writing the subsequent blocks until a new JPEG header is found
+    }
+    else if (output != NULL)
+    {
+      fwrite(buffer, 1, 512, output);
     }
   }
-  // fread for reading data from memory card: fread(data, size, number, inptr)
-  // Buffer could be an array (define it)
 
+  fclose(output);
   fclose(input_file);
-  printf("Data written to the file succefully.\n");
+  printf("Data written to the files successfully.\n");
   return 0;
 }
