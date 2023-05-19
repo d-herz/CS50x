a5af3194f83b4ca90213fe99cbc57945db01bdf2
Daniel Hyres <danhyres@gmail.com>
2023-05-19 17:17:36 -0400

    Linked list update

--- week5/linked-list.c
+++ week5/linked-list.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Defining a "node" structure
+// It is self-referential, so we need to include the "struct node" inside
 typedef struct node
 {
   int number;
@@ -9,10 +11,13 @@ typedef struct node
 
 int main(int argc, char *argv[])
 {
+  // Declaring a linked-list of size 0
   node *list = NULL;
 
+  // for loop starting at 1 (because argv[0] is program name)
   for (int i = 1; i < argc; i++)
   {
+    // atoi() for converting ascii to integer
     int number = atoi(argv[i]);
 
     node *n = malloc(sizeof(node));
@@ -25,5 +30,17 @@ int main(int argc, char *argv[])
 
     n->next = list;
     list = n;
+  }
+
+  // temporary variable for iterating over the list in a while loop
+  // it starts off pointing at the first node in the list
+  node *ptr = list;
+
+  while (ptr != NULL)
+  {
+    printf("%i\n", ptr->number);
+
+    // follow the arrow and grab the "next" pointer each time
+    ptr = ptr->next;
   }
 }
